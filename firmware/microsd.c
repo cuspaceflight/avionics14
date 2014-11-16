70ee4faf450677891d5933cf6ee40c38caf9ca74
Chi Pham <fempsci@gmail.com>
2014-11-16 23:55:58 +0000

    microsd: clarified some decisions in the interface provided by microsd.

--- firmware/microsd.c
+++ firmware/microsd.c
@@ -9,9 +9,11 @@ static bool_t microsd_card_init(FATFS* fs) {
 }
 
 /* TODO: Try initialising the sd until it works.
+ * We keep a statically allocated FATFS in here, which is the one that is used
+ * for all file system purposes.
  * Something something error handling/timeout
  */
-void microsd_card_try_init(FATFS* fs) {}
+void microsd_card_try_init(void) { static FATFS fs; }
 
 /* TODO: Unmount the file system, disconnect the SD.
  */
@@ -22,24 +24,27 @@ void microsd_card_deinit(void) {}
  * either reading or writing, but not support as many modes/error states as
  * the FatFS one does.
  */
-FRESULT microsd_open_file(FIL* fp, const TCHAR* path, BYTE mode) {
+SDRESULT microsd_open_file(FIL* fp, const char* path, BYTE mode) {
+    // convert char* path to TCHAR* path (should be the same by default)
     return 1;
 }
 
 /* TODO: close file in <fp>.
  */
-bool_t microsd_close_file(FIL* fp) {
+SDRESULT microsd_close_file(FIL* fp) {
     return false;
 }
 
 /* TODO: Write <btw> bytes from <buf> to <fp>.
  */
-bool_t microsd_write(FIL* fp, const void* buf, UINT btw) {
+SDRESULT microsd_write(FIL* fp, const void* buf, unsigned int btw) {
+    // convert unsigned int to UINT
     return FALSE;
 }
 
 /* TODO: Read <btr> bytes from <fp> to <buf>.
  */
-bool_t microsd_read(FIL* fp, void* buf, UINT btr) {
+SDRESULT microsd_read(FIL* fp, void* buf, unsigned int btr) {
+    // convert unsigned int to UINT
     return FALSE;
 }
--- firmware/microsd.h
+++ firmware/microsd.h
@@ -1,13 +1,30 @@
-/* This is an abstracted "device driver" for the micro sd card.
- * (mostly just a layer upon the ChibiOS SDC driver)
+/* This is an abstracted "device driver" for the micro sd card and file system,
+ * mostly acting as a file system access layer
+ * (based on the ChibiOS SDC driver).
+ *
+ * It would be nice to have this module be the only dependency on SDC driver
+ * and FatFS, ie. full encapsulation. Currently, it is only FIL that requires
+ * users of this module to include ff.h, but that should be fixable ...
  */
+
+/* Literally FRESULT, with irrelevant (?) error states grouped into one */
+typedef enum {
+    SD_OK,
+    SD_NO_FILE,
+    SD_EXIST,
+    SD_ERR
+} SDRESULT;
+
+typedef enum {
+    SD_READ,
+    SD_WRITE,
+    SD_CREATE_NEW
+} SDMODE;
 
 /* Connect to sd card, mount file system
  * (keeps trying until it succeeds ... implement time out?)
- * <fs> is the file system object to be mounted (does this have to be the same
- * across usages, or is it fine to have one for datalogging, one for control?)
  */
-void microsd_card_try_init(FATFS* fs);
+void microsd_card_try_init(void);
 
 /* Unmount file system, disconnect sd, assumes no open files.
  */
@@ -15,24 +32,23 @@ void microsd_card_deinit(void);
 
 /* Open file on file system. For now, exact same signature as the FatFS one
  * because we need to be able to specify mode and see error results...
- * TODO: limit error modes (to OK, ERR, (EXIST)) and open modes (to READ,WRITE)
  */
-FRESULT microsd_open_file(FIL* fp, const TCHAR* path, BYTE mode);
+SDRESULT microsd_open_file(FIL* fp, const char* path, SDMODE mode);
 
 /* Closes file on file system. */
-bool_t microsd_close_file(FIL* fp);
+SDRESULT microsd_close_file(FIL* fp);
 
 /* Assumes sd is connected and mounted, file is open
  * (possibly needs some indicator that it isn't, ie. in the return value)
  * <fp> is the file to be written to
  * <buff> is the buffer containing the data to be written
- * <btw> is the number of bytes to write
+ * <btw> is the number of bytes to write. TODO: possibly smaller type?
  */
-bool_t microsd_write(FIL* fp, const void* buff, UINT btw); 
+SDRESULT microsd_write(FIL* fp, const void* buff, unsigned int btw);
 
 /* Assumes sd is connected and mounted, file is open
  * <fp> is the file being read from
  * <buf> is the buffer to write to
- * <btr> is the max number of bytes to read
+ * <btr> is the max number of bytes to read. TODO: possibly smaller type?
  */
-bool_t microsd_read(FIL* fp, void* buf, UINT btr);
+SDRESULT microsd_read(FIL* fp, void* buf, unsigned int btr);
