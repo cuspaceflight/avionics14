5a90840f2392f61af1c9c13c90d45cdd1ab2a3cb
darkefyre <darkefyre@users.noreply.github.com>
2014-11-28 15:08:20 +0000

    Added two channels for separation
    Also included an extra firing function to handle metrons as we need both to fire simultaneously and the current setup doesn't allow that.

--- firmware/pyro.c
+++ firmware/pyro.c
@@ -3,15 +3,20 @@
  * I've just defined a few different output channels
  * and have assumed we're using GPIO E (as Adam did)
  *
+ * NOTE: Separation requires 2 channels 
+ * (both input & output)
+ *
  * Output Channels:
  * GPIOE_PYRO_MAIN_F : for parachute. Pin 9
- * GPIOE_PYRO_SEPARATION_F : for separation. Pin 10
- * GPIOE_PYRO_DROGUE_F : for drogue. Pin 11
- * GPIOE_PYRO_SECOND_STAGE_F : for second stage. Pin 12
+ * GPIOE_PYRO_SEPARATION_1_F : for separation. Pin 10
+ * GPIOE_PYRO_SEPARATION_2_F : for separation. Pin 11
+ * GPIOE_PYRO_DROGUE_F : for drogue. Pin 12
+ * GPIOE_PYRO_SECOND_STAGE_F : for second stage. Pin 13
  *
  * Input Channels:
- * GPIOE_PYRO_MAIN_C : for parachute. Pin 5
- * GPIOE_PYRO_SEPARATION_C : for separation. Pin 6
+ * GPIOE_PYRO_MAIN_C : for parachute. Pin 4
+ * GPIOE_PYRO_SEPARATION_1_C : for separation. Pin 5
+ * GPIOE_PYRO_SEPARATION_2_C : for separation. Pin 6
  * GPIOE_PYRO_DROGUE_C : for drogue. Pin 7
  * GPIOE_PYRO_SECOND_STAGE_C : for second stage. Pin 8
  *
@@ -74,21 +79,23 @@ bool_t pyro_continuity_check()
          /* log the results */
          /* microsd_log_s16(CHAN_PYRO_C, p1, p2, p3, 0); */
          /* return an indication of whether all of them are connected */
-         return (p1 & p2 & p3) ;
+         return (p1 && p2 && p3) ;
     }
     else if (board_location == BOTTOM_BOARD)
     {
          p1 = int16_t(pyro_continuity(GPIOE_PYRO_MAIN_C));
-         p2 = int16_t(pyro_continuity(GPIOE_PYRO_SEPARATION_C));
+         p2 = int16_t(pyro_continuity(GPIOE_PYRO_SEPARATION_1_C));
+         p3 = int16_t(pyro_continuity(GPIOE_PYRO_SEPARATION_2_C));
          /* log the results */
          /* microsd_log_s16(CHAN_PYRO_C, p1, p2, p3, 0); */
          /* return an indication of whether all of them are connected */
-         return (p1 & p2) ;
+         return (p1 && p2 && p3) ;
     }
 
 
 /*
- * Fire the pyro output channels for a specified time
+ * Fire the e-match pyro output channels for a specified time
+ * NOTE: This is specifically for e-match channels, not metrons
  */
 void pyro_fire(uint8_t channel, uint16_t duration_ms)
 {
@@ -103,56 +110,101 @@ void pyro_fire(uint8_t channel, uint16_t duration_ms)
     chThdSleepMilliseconds(duration_ms);
 }
 
+/*
+ * Fire the metron pyro output channels for a specified time
+ * NOTE: This is specifically for metron channels, 
+ * not e-matches
+ */
+void pyro_fire_metron(uint8_t channel_1, uint8_t channel_2, 
+                      uint16_t duration_ms)
+{
+    palSetPad(GPIOE, channel_1);
+    palSetPad(GPIOE, channel_2);
+
+    /* Log this event */
+    /* microsd_log_s16(CHAN_PYRO_F, channel_1, 0); */
+    /* microsd_log_s16(CHAN_PYRO_F, channel_2, 0); */
+    chThdSleepMilliseconds(duration_ms);
+
+    palSetPad(GPIOE, channel_1);
+    palSetPad(GPIOE, channel_2);
+
+    chThdSleepMilliseconds(duration_ms);
+}
+
 /* SPECIFIC FIRING FUNCTIONS
  * Keep firing the circuit until continuity of that
  * particular firing channel breaks.
  * This indicates a successful fire.
  */
 
-/* Fire the drogue deployment pyros */
+/* Fire the drogue deployment pyros 
+ * This is a simple e match, so try firing 10 times.
+ * E matches require a DC step, so fire the channel
+ * for a reasonable time- 50 ms (TBC)
+ * If continuity breaks, stop firing
+ */
 void pyro_fire_drogue()
 {
     int i;
-    for(i=0; i<50; i++) 
+    for(i=0; i<10; i++) 
     {  
-        pyro_fire(GPIOE_PYRO_DROGUE, 9) ;
-        if(!pyro_continuity(GPIOE_PYRO_DROGUE))
+        pyro_fire(GPIOE_PYRO_DROGUE_F, 50) ;
+        if(!pyro_continuity(GPIOE_PYRO_DROGUE_C))
             break;
     }
 }
 
-/* Fire  main chute deployment pyro */
+/* Fire  main chute deployment pyro
+ * This is a simple e match, so try firing 10 times.
+ * E matches require a DC step, so fire the channel
+ * for a reasonable time- 50 ms (TBC)
+ * If continuity breaks, stop firing
+ */
 void pyro_fire_main()
 {
     int i;
-    for(i=0; i<50; i++) 
+    for(i=0; i<10; i++) 
     {  
-        pyro_fire(GPIOE_PYRO_MAIN, 9) ;
-        if(!pyro_continuity(GPIOE_PYRO_MAIN))
+        pyro_fire(GPIOE_PYRO_MAIN_F, 50) ;
+        if(!pyro_continuity(GPIOE_PYRO_MAIN_C))
             break;
     }
 }
 
-/* Fire  separation pyro */
+/* Fire  separation pyro 
+ * This involves 2 metrons, so try firing 50 times.
+ * Firing involves a pulsed input
+ * for a reasonable time- 10 ms 
+ * If continuity breaks, stop firing
+ */
 void pyro_fire_separation()
 {
     int i;
     for(i=0; i<50; i++) 
     {  
-        pyro_fire(GPIOE_PYRO_SEPARATION, 9) ;
-        if(!pyro_continuity(GPIOE_PYRO_SEPARATION))
+        pyro_fire_metron(GPIOE_PYRO_SEPARATION_1_F, 
+        GPIOE_PYRO_SEPARATION_2_F, 10) ;
+        
+        if(!pyro_continuity(GPIOE_PYRO_SEPARATION_1_C)
+        && !pyro_continuity(GPIOE_PYRO_SEPARATION_2_C)) 
             break;
     }
 }
 
-/* Fire  second stage pyro */
+/* Fire  second stage pyro
+ * This is a simple e match, so try firing 10 times.
+ * E matches require a DC step, so fire the channel
+ * for a reasonable time- 50 ms (TBC)
+ * If continuity breaks, stop firing
+ */
 void pyro_fire_second_stage()
 {
     int i;
-    for(i=0; i<50; i++) 
+    for(i=0; i<10; i++) 
     {  
-        pyro_fire(GPIOE_PYRO_SECOND_STAGE, 9) ;
-        if(!pyro_continuity(GPIOE_PYRO_SECOND_STAGE))
+        pyro_fire(GPIOE_PYRO_SECOND_STAGE_F, 50) ;
+        if(!pyro_continuity(GPIOE_PYRO_SECOND_STAGE_C))
             break;
     }
 }
