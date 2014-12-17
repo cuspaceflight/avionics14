1364d489ad055539e02797ee492d57e37a2138d0
darkefyre <darkefyre@users.noreply.github.com>
2014-12-17 17:49:19 +0000

    Update hmc5883l.c

--- firmware/hmc5883l.c
+++ firmware/hmc5883l.c
@@ -46,13 +46,13 @@ static bool_t hmc5883l_transmit(uint8_t *buf)
 static bool_t hmc5883l_receive(uint8_t *buf, int8_t *buf_data)
 {
     msg_t rv;
-	buf[0] = 0x06;
-	buf[1] = NULL;
+    buf[0] = 0x06;
+    buf[1] = NULL;
 	
 	/* Timeout is somewhat arbitrary- CHECK THIS */
 	
     rv = i2cMasterTransmitTimeout(&I2CD1, HMC5883L_I2C_READ_ADDR, buf, 
-	                              1, buf_data, 6, 100);
+	                          1, buf_data, 6, 100);
     
 	if  (rv == RDY_OK)
 	    return TRUE;
@@ -62,7 +62,7 @@ static bool_t hmc5883l_receive(uint8_t *buf, int8_t *buf_data)
 
 static bool_t hmc5883l_init(uint8_t *buf, int8_t *buf_data)
 {
- 	bool_t success;
+    bool_t success;
   
     /* Configure Reg A - Highest Data Rate- 75 Hz */
     buf[0] = 0x00;
@@ -74,13 +74,12 @@ static bool_t hmc5883l_init(uint8_t *buf, int8_t *buf_data)
     buf[1] = 0x00;
     success &= hmc5883l_transmit(buf) ;
 	
-	/* Wait for 20 ms whilst measurements are taken, then read them */
-	
-	chThdSleepMilliseconds(20) ;
-	
-	success &= hmc5883l_receive(buf, buf_data) ;
-	
-	return success;
+    /* Wait for 20 ms whilst measurements are taken, then read them */
+    chThdSleepMilliseconds(20) ;
+    
+    success &= hmc5883l_receive(buf, buf_data) ;
+
+    return success;
   
 }
 
@@ -96,14 +95,14 @@ static void hmc5883l_field_convert(int8_t *buf_data, float *field)
 {
     int16_t temp ;
 	
-	temp = (buf_data[0] << 8 | buf_data[1] );
-	field[0] = (float) temp * counts_to_Tesla;
+    temp = (buf_data[0] << 8 | buf_data[1] );
+    field[0] = (float) temp * counts_to_Tesla;
 	
-	temp = (buf_data[2] << 8 | buf_data[3] );
-	field[1] = (float) temp * counts_to_Tesla;	
-	
-	temp = (buf_data[4] << 8 | buf_data[5] ) ;
-	field[2] = (float) temp * counts_to_Tesla;
+    temp = (buf_data[2] << 8 | buf_data[3] );
+    field[1] = (float) temp * counts_to_Tesla;	
+    
+    temp = (buf_data[4] << 8 | buf_data[5] ) ;
+    field[2] = (float) temp * counts_to_Tesla;
 }
 
 /* 
@@ -114,9 +113,9 @@ void hmc5883l_wakeup(EXTDriver *extp, expchannel_t channel)
 {
     (void)extp;
     (void)channel;
-	chSysLockFromIsr();
+    chSysLockFromIsr();
     if(tpHMC5883L != NULL) 
-	{
+    {
         chSchReadyI(tpHMC5883L);
         tpHMC5883L = NULL;
     }
@@ -126,14 +125,14 @@ void hmc5883l_wakeup(EXTDriver *extp, expchannel_t channel)
 msg_t hmc5883l_thread(void *arg)
 {
     (void)arg;
-	const int bufsize_1 = 2;
-	const int bufsize_2 = 6;
-
-	uint8_t buf[bufsize_1];
-	int8_t buf_data[bufsize_2];
-	float field[3] ;
+    const int bufsize_1 = 2;
+    const int bufsize_2 = 6;
+    
+    uint8_t buf[bufsize_1];
+    int8_t buf_data[bufsize_2];
+    float field[3] ;
 	
-	chRegSetThreadName("HMC5883L");
+    chRegSetThreadName("HMC5883L");
 	
 /* 
  * Reset Magno so it's in a known state 
@@ -141,16 +140,16 @@ msg_t hmc5883l_thread(void *arg)
  */
 
     /* 
-	 * palClearPad(GPIOB, GPIOB_GPS_RESET); 
+     * palClearPad(GPIOB, GPIOB_GPS_RESET); 
      * chThdSleepMilliseconds(100);
      * palSetPad(GPIOB, GPIOB_GPS_RESET); 
      * chThdSleepMilliseconds(500);
-	 */
+     */
 
     i2cStart(&I2CD1, &i2cconfig);
 
     if(!hmc5883l_init(buf, buf_data) 
-	{
+    {
         while(1) chThdSleepMilliseconds(5);
     }
     
@@ -161,11 +160,11 @@ msg_t hmc5883l_thread(void *arg)
 		if (hmc5883l_receive(buf, buf_data))
 		{
 		    hmc5883l_field_convert(buf_data, field);
-			microsd_log_s16(CHAN_IMU_MAGNO /*define*/, 
-			                field[0], field[1], field[2], 0);
-			/*define this state estimation function */
-            state_estimation_new_magno(field[0], 
-			                           field[1], field[2]);
+		    microsd_log_s16(CHAN_IMU_MAGNO /*define*/, 
+			            field[0], field[1], field[2], 0);
+		    /*define this state estimation function */
+                    state_estimation_new_magno(field[0], 
+			                       field[1], field[2]);
 		}
 		
 		else
